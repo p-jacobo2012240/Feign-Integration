29680455770721b6f428a28e247bd4b796f027bb
p-jacobo2012240 <pablojacobo1998@gmail.com>
2022-01-09 17:26:45 -0600

    refactor folders

--- src/main/java/com/hck/client_microservice/configuration/RepositoryConfiguration.java
+++ src/main/java/com/hck/client_microservice/configuration/RepositoryConfiguration.java
@@ -1,2 +1,9 @@
-package com.hck.client_microservice.configuration;public class RepositoryConfiguration {
+package com.hck.client_microservice.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.hck.client_microservice.repository")
+public class RepositoryConfiguration {
 }
